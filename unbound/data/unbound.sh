02f47eb3abaf54ae28c6dd7eaa7c9479742c03ab
Jonathan Peterson <me@eonasdan.com>
2024-01-30 11:32:13 -0500

    pr #48

--- unbound/data/unbound.sh
+++ unbound/data/unbound.sh
@@ -370,7 +370,7 @@ fi
 
 mkdir -p /opt/unbound/etc/unbound/dev && \
 cp -a /dev/random /dev/urandom /dev/null /opt/unbound/etc/unbound/dev/
-
+touch /opt/unbound/etc/unbound/unbound.log && \
 mkdir -p -m 700 /opt/unbound/etc/unbound/var && \
 chown _unbound:_unbound /opt/unbound/etc/unbound/var && \
 chown _unbound:_unbound /opt/unbound/etc/unbound/unbound.log && \
